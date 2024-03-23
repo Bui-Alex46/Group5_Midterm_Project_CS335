07b51327a321467c9bf0d5b4625a1864cbee44c0
Bui-Alex46 <alex46bui@gmail.com>
2024-03-23 09:52:42 -0700

    Adding starter code and other files

---
+++ Project1_starter.py
@@ -0,0 +1,20 @@
+
+
+
+def main():
+ 
+    # Person 1 schedule
+    person1_Schedule = [['7:00', '8:30'], ['12:00', '13:00'], ['16:00', '18:00']]
+    # Person 1 login/logout time
+    person1_daily_act = ['9:00', '19:00']
+
+
+    # Person 2 schedule
+    person2_Schedule = [['9:00', '10:30'], ['12:20','13:30'], ['14:00', '15:00'], ['16:00','17:00' ]]
+    # Person 2 login/logout time
+    person2_daily_act = ['9:00', '18:30']
+
+    duration = 30
+# Check if this file is being run as the main program
+if __name__ == "__main__":
+    main()  # Call the main function if this file is executed directly
